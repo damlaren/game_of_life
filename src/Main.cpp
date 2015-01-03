6573f99e0b2a757f692cc44793a86294026907c8
David McLaren <damlaren@gmail.com>
2015-01-03 12:37:08 -0800

    added a tick button, but it's huge

--- src/Main.cpp
+++ src/Main.cpp
@@ -13,84 +13,32 @@ class wxImagePanel : public wxPanel
 {
     wxBitmap resized;
     int w, h;
-    Board *board;
+    Board *mBoard;
 
 public:
-    wxImagePanel(wxFrame* parent, wxString file, wxBitmapType format);
+    wxImagePanel(Board *board, wxFrame* parent, wxString file, wxBitmapType format);
 
     void paintEvent(wxPaintEvent & evt);
     void paintNow();
     void OnSize(wxSizeEvent& event);
     void render(wxDC& dc);
 
-    void mouseDown(wxMouseEvent& event);
-
-    void loadBoard(const char* fileName);
-
-    // some useful events
-    /*
-    void mouseMoved(wxMouseEvent& event);
-    void mouseWheelMoved(wxMouseEvent& event);
-    void mouseReleased(wxMouseEvent& event);
-    void rightClick(wxMouseEvent& event);
-    void mouseLeftWindow(wxMouseEvent& event);
-    void keyPressed(wxKeyEvent& event);
-    void keyReleased(wxKeyEvent& event);
-    */
-
     DECLARE_EVENT_TABLE()
 };
 
 
 BEGIN_EVENT_TABLE(wxImagePanel, wxPanel)
-// some useful events
-/*
-EVT_MOTION(wxImagePanel::mouseMoved)
-EVT_LEFT_UP(wxImagePanel::mouseReleased)
-EVT_RIGHT_DOWN(wxImagePanel::rightClick)
-EVT_LEAVE_WINDOW(wxImagePanel::mouseLeftWindow)
-EVT_KEY_DOWN(wxImagePanel::keyPressed)
-EVT_KEY_UP(wxImagePanel::keyReleased)
-EVT_MOUSEWHEEL(wxImagePanel::mouseWheelMoved)
-*/
-
-// catch paint events
-EVT_PAINT(wxImagePanel::paintEvent)
-//Size event
-EVT_SIZE(wxImagePanel::OnSize)
-//TODO for now click to update
-EVT_LEFT_DOWN(wxImagePanel::mouseDown)
+    EVT_PAINT(wxImagePanel::paintEvent) // catch paint events
+    EVT_SIZE(wxImagePanel::OnSize)      // size event
 END_EVENT_TABLE()
 
-
-// some useful events
-/*
-void wxImagePanel::mouseMoved(wxMouseEvent& event) {}
-void wxImagePanel::mouseWheelMoved(wxMouseEvent& event) {}
-void wxImagePanel::mouseReleased(wxMouseEvent& event) {}
-void wxImagePanel::rightClick(wxMouseEvent& event) {}
-void wxImagePanel::mouseLeftWindow(wxMouseEvent& event) {}
-void wxImagePanel::keyPressed(wxKeyEvent& event) {}
-void wxImagePanel::keyReleased(wxKeyEvent& event) {}
-*/
-
-wxImagePanel::wxImagePanel(wxFrame* parent, wxString file, wxBitmapType format) :
+wxImagePanel::wxImagePanel(Board* board, wxFrame* parent, wxString file, wxBitmapType format) :
 wxPanel(parent)
 {
     w = -1;
     h = -1;
 
-    // make a test board
-    board = new BasicBoard(100, 100);
-    board->loadBoard("../../input/glider.txt");
-    //board->loadBoard("../../input/nova.txt");
-}
-
-void wxImagePanel::mouseDown(wxMouseEvent& event)
-{
-    board->update();
-    Refresh();
-    Update();
+    mBoard = board;
 }
 
 /*
@@ -122,7 +70,7 @@ void wxImagePanel::render(wxDC&  dc)
     dc.GetSize(&neww, &newh);
 
     int boardWidth, boardHeight;
-    const char* bits = board->getBitmap(boardWidth, boardHeight);
+    const char* bits = mBoard->getBitmap(boardWidth, boardHeight);
 
     //resized = wxBitmap(image.Scale(neww, newh /*, wxIMAGE_QUALITY_HIGH*/));
     resized = wxBitmap(wxBitmap(bits, boardWidth, boardHeight).ConvertToImage().Scale(neww, newh));
@@ -141,44 +89,69 @@ void wxImagePanel::OnSize(wxSizeEvent& event){
 }
 
 /**
+ * Button IDs
+ */
+enum
+{
+    BUTTON_TICK = wxID_HIGHEST + 1
+};
+
+/**
  * A frame to collect all window elements together.
  */
 class GOLFrame : public wxFrame
 {
 protected:
-    wxImagePanel * drawPane;
+    wxImagePanel* drawPane;
+    Board* mBoard;
 
 public:
-    // Button IDs
-    enum
-    {
-        BUTTON_TICK = wxID_HIGHEST + 1
-    };
-
-    GOLFrame(wxWindow *parent, wxWindowID id, const wxString& title, const wxPoint& pos, const wxSize& size)
+    GOLFrame(Board *board, wxWindow *parent, wxWindowID id, const wxString& title,
+        const wxPoint& pos, const wxSize& size)
         : wxFrame(parent, id, title, pos, size)
     {
+        mBoard = board;
     }
 
-    bool OnInit()
+    bool initialize()
     {
         // make sure to call this first
         wxInitAllImageHandlers();
 
         wxBoxSizer* sizer = new wxBoxSizer(wxHORIZONTAL);
-        
-        drawPane = new wxImagePanel(this, wxT("image.jpg"), wxBITMAP_TYPE_JPEG);
+
+        drawPane = new wxImagePanel(mBoard, this, wxT("image.jpg"), wxBITMAP_TYPE_JPEG);
         sizer->Add(drawPane, 1, wxEXPAND);
 
         // add buttons to control application
-        //wxButton *tickButton =
+        wxButton *tickButton = new wxButton(this, BUTTON_TICK, _T("Tick"), wxDefaultPosition, wxDefaultSize, 0);
+        sizer->Add(tickButton, 1, wxEXPAND);
 
         this->SetSizer(sizer);
 
         this->Show();
         return true;
     }
+
+    /// Move to next step of simulation
+    void tick()
+    {
+        mBoard->update();
+        drawPane->Refresh();
+        drawPane->Update();
+    }
+
+    void OnTickClick(wxCommandEvent& event)
+    {
+        tick();
+    }
+
+    DECLARE_EVENT_TABLE()
 };
+
+BEGIN_EVENT_TABLE(GOLFrame, wxFrame)
+    EVT_BUTTON(BUTTON_TICK, GOLFrame::OnTickClick)
+END_EVENT_TABLE()
 
 /**
  * Application to launch everything.
@@ -187,12 +160,18 @@ class GOLApp : public wxApp
 {
 protected:
     GOLFrame *frame;
+    Board *board;
 
 public:
     bool OnInit()
     {
-        frame = new GOLFrame(NULL, wxID_ANY, wxT("Hello wxDC"), wxPoint(50, 50), wxSize(800, 600));
-        return frame->OnInit();
+        // make a test board
+        board = new BasicBoard(100, 100);
+        board->loadBoard("../../input/glider.txt");
+        //board->loadBoard("../../input/nova.txt");
+
+        frame = new GOLFrame(board, NULL, wxID_ANY, wxT("Hello wxDC"), wxPoint(50, 50), wxSize(800, 600));
+        return frame->initialize();
     }
 };
 
